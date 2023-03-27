8f1009a51cc6071612e169b70736a32cad229926
elliotmc02 <elliotmoyanocutler2002@gmail.com>
2023-03-27 10:00:04 +0200

    Metodo fibonacci (no lo habia guardado)

--- com.entornos.elliotmoyanocutler.algoritmos/src/com/entornos/elliotmoyanocutler/algoritmos/algoritmos.java
+++ com.entornos.elliotmoyanocutler.algoritmos/src/com/entornos/elliotmoyanocutler/algoritmos/algoritmos.java
@@ -2,4 +2,9 @@ package com.entornos.elliotmoyanocutler.algoritmos;
 
 public abstract class algoritmos {
 
+	public int fibonacci(int numero) {
+		if (numero < 2)
+			return numero;
+		return fibonacci(numero - 1) + fibonacci(numero - 2);
+	}
 }
